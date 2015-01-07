046c2fb9a2562f1031dec24c126511b1bf2876b3
Nican <nican@nican.net>
2015-01-06 23:49:32 -0800

    Never commit build files.

--- gazebo/build/CMakeFiles/kratos_telemetry_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "kratos_telemetry_automoc.cpp"
-  "CMakeFiles/kratos_telemetry_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/kratos_telemetry_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
