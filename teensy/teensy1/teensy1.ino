d8ab0a8aac054d4f32ecdc86e4a926b39b832d56
Eric Schneider <franz.eric.schneider@gmail.com>
2015-02-17 16:52:46 -0500

    Tested the sorter/collector motors

--- teensy/teensy1/teensy1.ino
+++ teensy/teensy1/teensy1.ino
@@ -9,6 +9,7 @@
  * http://www.pjrc.com/teensy/td_libs_Encoder.html
  */
 
+
 #include <TimerOne.h>
 #include <Encoder.h>
 #include <Servo.h>
@@ -18,17 +19,19 @@
 #define LEFT_CMD_IN      11  // Pin 2 on the receiver
 #define RIGHT_CMD_IN     12  // Pin 3 on the receiver
 #define AUTO_SWITCH_IN   10  // Pin 5 on the receiver
-#define LEFT_OUT_PIN     2
-#define RIGHT_OUT_PIN    3
-#define PAUSE_SWITCH_OUT 13  // Goes to the switch nMOS gate
+#define LEFT_OUT_PIN     3   // PWM control on the left wheel
+#define RIGHT_OUT_PIN    2   // PWM control on the right wheel
+#define SORTER_OUT_PIN   4   // PWM control on the sorter
+#define COLLECTOR_OUT_A  5   // Goes to one side of the collector
+#define COLLECTOR_OUT_B  6   // Goes to one side of the collector
 
 
 int isSystemPaused = 0;
 int isSystemAuto = 0;
 
 // ENCODER SETUP
-Encoder wheelLeft(14, 15);
-Encoder wheelRight(16, 17);
+Encoder wheelLeft(17, 16);
+Encoder wheelRight(14, 15);
 const int ticksPerRev = 23330;  // Determined experimentally
 const int encoderHistLength = 5;
 volatile long oldLeft[encoderHistLength] = {-999, -999, -999, -999, -999};
@@ -39,6 +42,7 @@ volatile int rightVelocity = 0;
 // DRIVE SETUP
 Servo servoLeft;
 Servo servoRight;
+Servo servoSorter;
 int lastLeftCmd;
 int lastRightCmd;
 
@@ -50,107 +54,117 @@ const int MIN_SIGNAL = 1100;
 const int IN_DEADBAND = 100;
 const int OUT_DEADBAND = 2;
 const float INTERP_SLOPE = (MAX_WHEEL_SPEED - MIN_WHEEL_SPEED) /
-                           (float)(MAX_SIGNAL - MID_SIGNAL);
+                                                     (float)(MAX_SIGNAL - MID_SIGNAL);
 const float INTERP_OFFSET = MIN_WHEEL_SPEED - (MID_SIGNAL * INTERP_SLOPE);
 
+
 void setup() {
-  Timer1.initialize(VELOCITY_PERIOD_MICRO);
-  Timer1.attachInterrupt(calculateVelocity);
-  Serial.begin(9600);
-  pinMode(LEFT_CMD_IN, INPUT);
-  pinMode(RIGHT_CMD_IN, INPUT);
-  pinMode(AUTO_SWITCH_IN, INPUT);
-  pinMode(PAUSE_SWITCH_OUT, OUTPUT);
-  servoLeft.attach(LEFT_OUT_PIN); servoLeft.write(MIN_WHEEL_SPEED);
-  lastLeftCmd = MIN_WHEEL_SPEED;
-  servoRight.attach(RIGHT_OUT_PIN); servoRight.write(MIN_WHEEL_SPEED);
-  lastRightCmd = MIN_WHEEL_SPEED;
+    Timer1.initialize(VELOCITY_PERIOD_MICRO);
+    Timer1.attachInterrupt(calculateVelocity);
+    Serial.begin(9600);
+    pinMode(LEFT_CMD_IN, INPUT);
+    pinMode(RIGHT_CMD_IN, INPUT);
+    pinMode(AUTO_SWITCH_IN, INPUT);
+    pinMode(COLLECTOR_OUT_A, OUTPUT);
+    digitalWrite(COLLECTOR_OUT_A, LOW);
+    pinMode(COLLECTOR_OUT_B, OUTPUT);
+    digitalWrite(COLLECTOR_OUT_B, LOW);
+    servoLeft.attach(LEFT_OUT_PIN); servoLeft.write(MIN_WHEEL_SPEED);
+    lastLeftCmd = MIN_WHEEL_SPEED;
+    servoRight.attach(RIGHT_OUT_PIN); servoRight.write(MIN_WHEEL_SPEED);
+    lastRightCmd = MIN_WHEEL_SPEED;
+    servoSorter.attach(SORTER_OUT_PIN); servoSorter.write(MIN_WHEEL_SPEED);
 }
+
 
 void loop() {
-  // DEAL WITH COMPUTER COMMANDS
-  if (switchOn(AUTO_SWITCH_IN)) {
-    // TODO: Fill with computer commands
-    if (!isSystemAuto) {
-      servoLeft.write(MIN_WHEEL_SPEED);
-      servoRight.write(MIN_WHEEL_SPEED);
-      isSystemAuto = 1;
+    // DEAL WITH COMPUTER COMMANDS
+    if (switchOn(AUTO_SWITCH_IN)) {
+        // TODO: Fill with computer commands
+        if (!isSystemAuto) {
+            servoLeft.write(MIN_WHEEL_SPEED);
+            servoRight.write(MIN_WHEEL_SPEED);
+            isSystemAuto = 1;
+        }
     }
-  }
-  else {
-    lastLeftCmd = passThroughRC(servoLeft, LEFT_CMD_IN, lastLeftCmd);
-    lastRightCmd = passThroughRC(servoRight, RIGHT_CMD_IN, lastRightCmd);
-    if (isSystemAuto) {
-      isSystemAuto = 0;
+    else {
+     lastLeftCmd = passThroughRC(servoLeft, LEFT_CMD_IN, lastLeftCmd);
+     lastRightCmd = passThroughRC(servoRight, RIGHT_CMD_IN, lastRightCmd);
+        if (isSystemAuto) {
+            isSystemAuto = 0;
+        }
     }
-  }
-
-  printDataToComputer();
+    printDataToComputer();
 }
+
 
 // Averages the last encoder counts and reports velocity
 void calculateVelocity(void) {
-  for (int i = encoderHistLength - 1; i > 0; i--) {
-    oldLeft[i] = oldLeft[i - 1];
-    oldRight[i] = oldRight[i - 1];
-  }
-  oldLeft[0] = wheelLeft.read();;
-  oldRight[0] = wheelRight.read();;
+    for (int i = encoderHistLength - 1; i > 0; i--) {
+        oldLeft[i] = oldLeft[i - 1];
+        oldRight[i] = oldRight[i - 1];
+    }
+    oldLeft[0] = wheelLeft.read();;
+    oldRight[0] = wheelRight.read();;
 
-  int leftSum = 0;
-  int rightSum = 0;
-  for (int i = 0; i < (encoderHistLength - 2); i++) {
-    leftSum += oldLeft[i] - oldLeft[i + 1];
-    rightSum += oldRight[i] - oldRight[i + 1];
-  }
-
-  leftVelocity = leftSum / (encoderHistLength - 1);
-  rightVelocity = rightSum / (encoderHistLength - 1);
+    int leftSum = 0;
+    int rightSum = 0;
+    for (int i = 0; i < (encoderHistLength - 2); i++) {
+        leftSum += oldLeft[i] - oldLeft[i + 1];
+        rightSum += oldRight[i] - oldRight[i + 1];
+    }
+    leftVelocity = leftSum / (encoderHistLength - 1);
+    rightVelocity = rightSum / (encoderHistLength - 1);
 }
+
 
 // Checks whether an RC switch is on/off and returns a boolean
 bool switchOn(int switchPin) {
-  int duration = pulseIn(switchPin, HIGH);
-  if (duration > MID_SIGNAL) {
-    return true;
-  }
-  return false;
+    int duration = pulseIn(switchPin, HIGH);
+    if (duration > MID_SIGNAL) {
+        return true;
+    }
+    return false;
 }
+
 
 // Interpolates the input RC values as servo values for the motors
 int passThroughRC(Servo servo, int pin, int lastCmd) {
-  int duration = pulseIn(pin, HIGH);
-  int cmd;
+    int duration = pulseIn(pin, HIGH);
+    int cmd;
 
-  // Brakes the robot when in the deadband
-  if (within(duration, MID_SIGNAL, IN_DEADBAND)) {
-    cmd = MIN_WHEEL_SPEED;
-  }
-  else {
-    // Adjusts the duration to prevent a jump on either side of the deadband
-    if (duration > MID_SIGNAL) { duration -= IN_DEADBAND; }
-    else { duration += IN_DEADBAND; }
-    cmd = INTERP_SLOPE * duration + INTERP_OFFSET;
-  }
-  
-  if (within(cmd, MIN_WHEEL_SPEED, (MAX_WHEEL_SPEED - MIN_WHEEL_SPEED))) {
-    servo.write(cmd);
-    return cmd;
-  }
-  return lastCmd;
+    // Brakes the robot when in the deadband
+    if (within(duration, MID_SIGNAL, IN_DEADBAND)) {
+        cmd = MIN_WHEEL_SPEED;
+    }
+    else {
+        // Adjusts the duration to prevent a jump on either side of the deadband
+        if (duration > MID_SIGNAL) { duration -= IN_DEADBAND; }
+        else { duration += IN_DEADBAND; }
+        cmd = INTERP_SLOPE * duration + INTERP_OFFSET;
+    }
+    
+    if (within(cmd, MIN_WHEEL_SPEED, (MAX_WHEEL_SPEED - MIN_WHEEL_SPEED))) {
+        servo.write(cmd);
+        return cmd;
+    }
+    else {
+        return lastCmd;
+    }
 }
+
 
 // Checks whether the given value is within a given offset from a goal value
 bool within(int value, int goal, int offset) {
-  if ((goal + offset > value) && (goal - offset < value)) {
-    return true;
-  }
-  return false;
+    if ((goal + offset > value) && (goal - offset < value)) {
+        return true;
+    }
+    return false;
 }
+
 
 void printDataToComputer() {
-  Serial.print("LVEL\t"); Serial.print(leftVelocity);
-  Serial.print("\tRVEL\t"); Serial.print(rightVelocity);
-  Serial.print("\tAUTO\t"); Serial.println(isSystemAuto);
+    Serial.print("LVEL,"); Serial.print(leftVelocity);
+    Serial.print(",RVEL,"); Serial.print(rightVelocity);
+    Serial.print(",AUTO,"); Serial.println(isSystemAuto);
 }
-
