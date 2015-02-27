5a4ac1d7a05bec1508a14b8114ad8dab311fd22c
Eric Schneider <franz.eric.schneider@gmail.com>
2015-02-27 11:07:53 -0500

    Made cpplint compliant

--- teensy/teensy1/accelerometer.ino
+++ teensy/teensy1/accelerometer.ino
@@ -1,4 +1,5 @@
 // Put professional header here
+// Include some type of copyright
 
 #include <Arduino.h>
 
@@ -7,14 +8,14 @@
 #define accelerometerYPin A8
 #define accelerometerZPin A7
 
-// TODO: Make more complex with conversions/filtering after testing
+// TODO(Eric): Make more complex with conversions/filtering after testing
 int readAccelerometerSingleAxis(int pin) {
     int val = analogRead(pin);
     return val;
 }
 
 void readAccelerometer(int *axes) {
-   axes[0] = readAccelerometerSingleAxis(accelerometerXPin);
-   axes[1] = readAccelerometerSingleAxis(accelerometerYPin);
-   axes[2] = readAccelerometerSingleAxis(accelerometerZPin);
+    axes[0] = readAccelerometerSingleAxis(accelerometerXPin);
+    axes[1] = readAccelerometerSingleAxis(accelerometerYPin);
+    axes[2] = readAccelerometerSingleAxis(accelerometerZPin);
 }
