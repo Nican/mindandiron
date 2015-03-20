9889a1eadc7dd5d155d03d121d7ad7152f9e54f6
Eric Schneider <franz.eric.schneider@gmail.com>
2015-03-20 09:58:10 -0400

    Made Arduino blink light when system is paused

--- arduino/arduino1/arduino1.ino
+++ arduino/arduino1/arduino1.ino
@@ -3,6 +3,7 @@
 
 
 #include <Herkulex.h>
+#include <TimerOne.h>
 
 #include "rc_includes.h"
 
@@ -11,8 +12,8 @@ const int camServoID = 253; //motor ID - verify your ID !
 #define TX 8
 #define RX 9
 
-
 int camServoSetpoint = 0;
+int ledState = LOW;
 
 
 void setup()
@@ -27,19 +28,31 @@ void setup()
     delay(200);
     Herkulex.moveOneAngle(camServoID, camServoSetpoint, 1000, LED_GREEN);
     delay(2000);
+
+    pinMode(LED_PIN_OUT, OUTPUT);
+    Timer1.initialize(1000000);  // Timer period in microseconds (max 8.3s)
+    Timer1.attachInterrupt(blinkLED); // blinkLED to run every 0.15 seconds
 }
 
 void loop(){
     readComputerCommands(&camServoSetpoint);
 
-    // TODO (Eric): Replace this with light that flashes when running, is on when paused
-    controlLight(!getPaused());
-
-    // if (getAuto()) {
-    Herkulex.moveOneAngle(camServoID, camServoSetpoint, 1000, LED_BLUE);
-    // } else {
-    //     Herkulex.moveOneAngle(camServoID, -159, 1000, LED_BLUE);
-    // }
+    if (getAuto()) {
+        Herkulex.moveOneAngle(camServoID, camServoSetpoint, 1000, LED_BLUE);
+    } else {
+        Herkulex.moveOneAngle(camServoID, 0, 1000, LED_BLUE);
+    }
 
     printDataToComputer(Herkulex.getAngle(camServoID), getPaused());
+}
+
+// Blinks the LED when paused, leaves it solidly on while running
+void blinkLED(void)
+{
+	if (ledState == LOW) {
+		ledState = HIGH;
+	} else if (getPaused()) {
+		ledState = LOW;
+	}
+	digitalWrite(LED_PIN_OUT, ledState);
 }
