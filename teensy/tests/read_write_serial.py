5602eadec09edc3ba33dac92e2943ec7451a4fd0
Eric Schneider <franz.eric.schneider@gmail.com>
2015-04-10 09:45:59 -0400

    Python tests of writing to the Arduino

--- teensy/tests/read_write_serial.py
+++ teensy/tests/read_write_serial.py
@@ -2,27 +2,27 @@ import serial
 
 
 def main():
-	ser = serial.Serial('/dev/ttyACM1', 9600)
-	counter = 0
-	cycleCounts = 50
-	Lcmd = 0
-	Rcmd = -0
-	Collector = 0
-	Sorter = 0
-	while(True):
-		print(ser.readline())
-		if counter % cycleCounts == 0:
-			print('WRITING DATA TO TEENSY')
-			ser.write('LVEL\t%s\tRVEL\t%s\tCOLL\t%s\tSORT\t%s\tEND'
-					  % (Lcmd, Rcmd, Collector, Sorter))
-			Lcmd += 10
-			if Lcmd > 160:
-				Lcmd = -160
-			Rcmd -= 10
-			if Rcmd < -160:
-				Rcmd = 160
-		counter += 1
+    ser = serial.Serial('/dev/ttyACM1', 9600)
+    counter = 0
+    cycleCounts = 50
+    Lcmd = 0
+    Rcmd = -0
+    Collector = 0
+    Sorter = 0
+    while(True):
+        print(ser.readline())
+        if counter % cycleCounts == 0:
+            print('WRITING DATA TO TEENSY')
+            ser.write('LVEL\t%s\tRVEL\t%s\tCOLL\t%s\tSORT\t%s\tEND'
+                      % (Lcmd, Rcmd, Collector, Sorter))
+            Lcmd += 10
+            if Lcmd > 160:
+                Lcmd = -160
+            Rcmd -= 10
+            if Rcmd < -160:
+                Rcmd = 160
+        counter += 1
 
 
 if __name__ == '__main__':
-	main()
+    main()
